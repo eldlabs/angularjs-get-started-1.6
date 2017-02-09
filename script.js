d8f5cf474d565df9cf9b7286ecc817f38527c070
Eldar Terzic <eldarterzic@Eldars-MacBook-Pro.local>
2017-02-09 16:08:52 +0100

    Adding service to project

--- script.js
+++ script.js
@@ -2,28 +2,49 @@
 
   var app = angular.module("githubViewer", []);
 
-  app.controller('MainController', function($scope, $http) {
+  app.controller('MainController', function($scope, github, $interval, $log, $anchorScroll, $location) {
 
-    var onUserComplete = function(response) {
-      $scope.user = response.data;
-      $http.get($scope.user.repos_url).then(onRepos, onError)
+    var onUserComplete = function(data) {
+      $scope.user = data;
+      github.getRepos($scope.user).then(onRepos, onError)
     };
     
-    var onRepos = function(response){
-      $scope.repos = response.data;
+    var onRepos = function(data){
+      $scope.repos = data;
+      $location.hash("userDetails");
+      $anchorScroll();
     }
 
     var onError = function(reason) {
       $scope.error = "Could not fetch the data.";
     }
     
+    var decrementCountdown = function(){
+      $scope.countdown -= 1;
+      if ($scope.countdown < 1) {
+        $scope.search($scope.username);
+      }
+    }
+    
+    var countdownInterval = null;
+    var startCountdown = function(){
+      countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+    }
+    
     $scope.search = function(username){
-      $http.get("https://api.github.com/users/" + username).then(onUserComplete, onError);
+      $log.info("Searching for " + username);
+      github.getUser(username).then(onUserComplete, onError);
+      if (countdownInterval) {
+        $interval.cancel(countdownInterval);
+        $scope.countdown = null;
+      }
     }
 
     $scope.message = "GitHub Viewer";
     $scope.repoSortOrder = "-stargazers_count";
-
+    $scope.countdown = 5;
+    startCountdown();
+    
   });
 
 }());
