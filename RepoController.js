a4786364060bdc0192a0a11fa1d89e2809314d7e
Eldar Terzic <eldarterzic@Eldars-MacBook-Pro.local>
2017-02-10 15:27:35 +0100

    Addin routing to solution

---
+++ RepoController.js
@@ -0,0 +1,22 @@
+(function() {
+
+    var app = angular.module("githubViewer");
+  
+    app.controller('RepoController', function($scope, $routeParams, github) {
+
+        var onRepo = function(data){
+          $scope.repo = data;  
+        };
+        
+        var onError = function(reason){
+          $scope.error = reason;  
+        };
+
+
+        var reponame = $routeParams.reponame;
+        var username = $routeParams.username;
+
+        github.getRepoDetails(username, reponame)
+              .then(onRepo, onError);
+    };
+}());
